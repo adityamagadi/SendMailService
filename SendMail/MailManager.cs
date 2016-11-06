c6b7553d8a5a6f12783c98678b624b843ded8083
adityamagadi <adityamagadi@gmail.com>
2016-11-06 18:56:19 +0530

    initial commit

---
+++ SendMail/MailManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace SendMail
+{
+    public static class MailManager
+    {
+        public static void sendMail(string toAddress)
+        {
+            MailMessage message = new MailMessage("adityamagadi@gmail.com", toAddress);
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = true;
+            client.Host = "smtp.gmail.com";
+            client.Credentials = new System.Net.NetworkCredential("adityamagadi@gmail.com", "Bullet35091#");
+            message.Subject = "test";
+            message.Body = "this is a test body";
+            client.Send(message);
+        }
+    }
+}
